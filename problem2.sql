810a83612f24a9a72a489caefbdab33252e80cd0
Oleg Zasukha <ozamericos@gmail.com>
2023-06-24 00:21:39 +0000

    PostgreSQL_Aggregate_functions

---
+++ problem2.sql
@@ -0,0 +1,19 @@
+       
+Select count(id)
+from employee 
+where last_name = 'Smith';
+        /**
+         * Problem 2: use the COUNT() aggregate function to output the number of employees with the last name "Smith".
+         * Note: the output should not contain any other info but the count.
+         */
+         /**
+ *   employee table
+ *   |  id  |   first_name   |   last_name   |  salary  |
+ *   --------------------------------------------------
+ *   |1     |'Steve'         |'Garcia'       |67400.00  |
+ *   |2     |'Alexa'         |'Smith'        |42500.00  |
+ *   |3     |'Steve'         |'Jones'        |99890.99  |
+ *   |4     |'Brandon'       |'Smith'        |120000    |
+ *   |5     |'Adam'          |'Jones'        |55050.50  |
+ *
+ */
